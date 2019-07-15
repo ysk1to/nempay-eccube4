6e7dd7a580d80ac1023259f955941ec78d306eb2
Yusuke Ito <maroemon58@gmail.com>
2019-07-15 19:14:39 +0900

    Update Config

---
+++ Entity/Config.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace Plugin\SimpleNemPay\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Eccube\Entity\AbstractEntity;
+
+/**
+ * Config
+ * 
+ * @ORM\Table(name="plg_simple_nem_pay_config")
+ * @ORM\Entity(repositoryClass="Plugin\SimpleNemPay\Repository\ConfigRepository")
+ */
+class Config extends AbstractEntity
+{
+
+    /**
+     * @var int
+     * 
+     * @ORM\Column(name="id", type="integer", options={"unsigned":true})
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @var integer
+     * 
+     * @ORM\Column(name="env", type="integer")
+     */
+    private $env;
+
+    /**
+     * @var string
+     * 
+     * @ORM\Column(name="seller_nem_addr", type="string", length=255, nullable=true)
+     */
+    private $seller_nem_addr;
+
+    /**
+     * Constructor
+     */
+    public function __construct()
+    { }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getEnv()
+    {
+        return $this->env;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setEnv($env)
+    {
+        $this->env = $env;
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getSellerNemAdd()
+    {
+        return $this->seller_nem_addr;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setSellerNemAddr($seller_nem_addr)
+    {
+        $this->seller_nem_addr = $seller_nem_addr;
+
+        return $this;
+    }
+}
