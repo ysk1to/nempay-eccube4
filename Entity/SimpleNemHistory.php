c3872328a6eb2776939964c5c6bae65d784b6fe3
Yusuke Ito <maroemon58@gmail.com>
2019-07-15 19:05:31 +0900

    Update NemHistory

---
+++ Entity/SimpleNemHistory.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace Plugin\SimpleNemPay\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * SimpleNemHistory
+ * 
+ * @ORM\Table(name="plg_simple_nem_pay_history")
+ * @ORM\InheritanceType("JOINED")
+ * @ORM\DiscriminatorColumn(name="discriminator_type", type="string", length=255)
+ * @ORM\HasLifecycleCallbacks()
+ * @ORM\Entity(repositoryClass="Plugin\SimpleNemPay\Repository\SimpleNemHistory")
+ */
+class SimpleNemHistory extends \Eccube\Entity\AbstractEntity
+{
+
+    /**
+     * @var integer
+     * 
+     * @ORM\Column(name="id", type="integer", options={"unsigned":true})
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @var Eccube\Entity\Order
+     * 
+     * @ORM\ManyToOne(targetEntity="Eccube\Entity\Order", inversedBy="SimpleNemHistory")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="order_id", referencedColumnName="id")
+     * })
+     */
+    private $Order;
+
+    /**
+     * @var string
+     * 
+     * @ORM\Column(name="transaction_id", type="string", length=255)
+     */
+    private $transaction_id;
+
+    /**
+     * @var float
+     * 
+     * @ORM\Column(name="amount", type="decimal", precision=12, scale=2)
+     */
+    private $amount;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setOrder(\Eccube\Entity\Order $Order)
+    {
+        $this->Order = $Order;
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getOrder()
+    {
+        return $this->Order;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setTransactionId($transactionId)
+    {
+        $this->transaction_id = $transactionId;
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getTransactionId()
+    {
+        return $this->transaction_id;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setAmount($amount)
+    {
+        $this->amount = $amount;
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAmount()
+    {
+        return $this->amount;
+    }
+}
