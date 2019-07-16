26eabbfd6d792086ff6150ee0be0a6d08eb2ae2a
Yusuke Ito <maroemon58@gmail.com>
2019-07-17 01:13:12 +0900

    Add TwigBlock file

---
+++ SimpleNemPayTwigBlock.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Plugin\SimpleNemPay;
+
+use Eccube\Common\EccubeTwigBlock;
+
+class SimpleNemPayTwigBlock implements EccubeTwigBlock
+{
+    /**
+     * @return array
+     */
+    public static function getTwigBlock()
+    {
+        return [];
+    }
+}
