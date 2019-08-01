b0ebe41c0c19e8ef455a05c1db4f7f27542cbdcb
Yusuke Ito <maroemon58@gmail.com>
2019-08-01 14:48:57 +0900

    Fix ConfigType

--- Form/Type/Admin/ConfigType.php
+++ Form/Type/Admin/ConfigType.php
@@ -8,6 +8,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints as Assert;
 
 class ConfigType extends AbstractType
