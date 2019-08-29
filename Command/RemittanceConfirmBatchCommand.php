228559316fd38dc81efe117d41d0231b72cedb96
Yusuke Ito <maroemon58@gmail.com>
2019-08-29 22:01:37 +0900

    Add remittance confirm command function

--- Command/RemittanceConfirmBatchCommand.php
+++ Command/RemittanceConfirmBatchCommand.php
@@ -2,57 +2,48 @@
 
 namespace Plugin\SimpleNemPay\Command;
 
-use Plugin\SimpleNemPay\Entity\NemOrder;
-use Plugin\SimpleNemPay\Entity\NemHistory;
-use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputArgument;
+use Doctrine\Bundle\DoctrineBundle\Command\DoctrineCommand;
+use Plugin\SimpleNemPay\Entity\Master\NemStatus;
+use Plugin\SimpleNemPay\Repository\Master\NemStatusRepository;
+use Plugin\SimpleNemPay\Service\NemRequestService;
+use Eccube\Repository\OrderRepository;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Input\InputOption;
 
 /**
  * 入金確認実行コマンド
- *
- * app/consoleに要追記
- * $console->add(new Plugin\SimpleNemPay\Command\RemittanceConfirmBatchCommand(new Eccube\Application()));
- *
- * crontab  ex. 0 * * * * /usr/bin/php /var/www/html/eccube-3.0.15/app/console simple_nempay:remittance_confirm
  */
-class RemittanceConfirmBatchCommand extends Command
+class RemittanceConfirmBatchCommand extends DoctrineCommand
 {
+    protected static $defaultName = 'simple_nem_pay:remittance_confirm';
 
-    private $app;
-
-    public function __construct(\Eccube\Application $app, $name = null)
-    {
-        parent::__construct($name);
-        $this->app = $app;
+    public function __construct(
+        NemStatusRepository $nemStatusRepository,
+        NemRequestService $nemRequestService,
+        OrderRepository $orderRepository
+    ) {
+        parent::__construct();
+        $this->nemStatusRepository = $nemStatusRepository;
+        $this->nemRequestService = $nemRequestService;
+        $this->orderRepository = $orderRepository;
     }
 
     protected function configure()
     {
-        $this->setName('simple_nempay:remittance_confirm')
-             ->setDescription('送金確認バッチ処理');
+        $this->setDescription('送金確認バッチ処理');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $this->app->initialize();
-        $this->app->initializePlugin();
-        $this->app->boot();
-
-        $softDeleteFilter = $this->app['orm.em']->getFilters()->getFilter('soft_delete');
-        $softDeleteFilter->setExcludes(array(
-            'Eccube\Entity\Order'
-        ));
-        
         // 対象の受注を取得
-        $arrNemOrder = $this->app['eccube.plugin.simple_nempay.repository.nem_order']->getOrderPayWaitForSimpleNemPay();
-        if (empty($arrNemOrder)) {
+        $NemStatus = $this->nemStatusRepository->find(NemStatus::PAY_WATING);
+        $Orders = $this->orderRepository->findBy(['NemStatus' => $NemStatus]);
+        if (empty($Orders)) {
             return;
         }
-        
-        $this->app['eccube.plugin.simple_nempay.service.nem_shopping']->confirmNemRemittance($arrNemOrder);
-    }
 
+        foreach ($Orders as $Order) {
+            $this->nemRequestService->confirmNemRemittance($Order);
+        }
+    }
 }
